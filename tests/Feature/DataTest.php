e8859eddb52c3a653cb0b664cfe53ef0c3e9195c
Gleb Starkov <glebstarkov@gmail.com>
2020-10-19 12:22:56 +0800

    Tests, Readme

--- tests/Feature/DataTest.php
+++ tests/Feature/DataTest.php
@@ -10,11 +10,33 @@ use App\Models\Services\JsonRpcClient;
 
 class DataTest extends TestCase
 {
+    /**
+     * Create test
+     *
+     * @return string
+     */
     public function testCreate()
     {
         $client = new JsonRpcClient();
         $page_uid = uniqid();
         $data = $client->send('create', ['page_uid' => $page_uid, 'title' => 'test', 'body' => 'test']);
         $this->assertTrue($data['result']['page_uid'] == $page_uid);
+
+        return $page_uid;
+    }
+
+    /**
+     * getPageById test
+     *
+     * @param string $page_uid
+     * @depends testCreate
+     *
+     * @return void
+     */
+    public function testGetPageById(string $page_uid)
+    {
+        $client = new JsonRpcClient();
+        $data = $client->send('getPageById', ['page_uid' => $page_uid]);
+        $this->assertTrue($data['result']['title'] == 'test');
     }
 }
